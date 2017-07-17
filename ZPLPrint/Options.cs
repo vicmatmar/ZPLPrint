efcc63493c194c31b7ab08c01f391d1861f929d5
Victor Martin <victormartin@centralite.com>
2017-07-17 16:48:31 -0500

    Print label pprovided separate zebra and data

---
+++ ZPLPrint/Options.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommandLine;
+using CommandLine.Text;
+
+
+namespace ZPLPrint
+{
+    class Options
+    {
+        [Option('a', "PrinterAddr", Required = true,
+            HelpText = "Printer Address")]
+        public string Printer_Address { get; set; }
+
+        [Option('p', "PrinterPort", Required = false, DefaultValue = 9100,
+            HelpText = "Printer Port")]
+        public int Printer_Port { get; set; }
+
+        [Option('z', "ZPLFile", Required = true,
+            HelpText = "Zebra output formated file")]
+        public string ZPL_File { get; set; }
+
+        [Option('d', "DataFile", Required = false,
+            HelpText = "Data file (One data item per line).")]
+        public string Data_File { get; set; }
+
+    }
+}
--- ZPLPrint/Program.cs
+++ ZPLPrint/Program.cs
@@ -4,12 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+
+
+using System.Net;
+
 namespace ZPLPrint
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+
+            var options = new Options();
+            var isValid = CommandLine.Parser.Default.ParseArguments(args, options);
+
+            if (!isValid)
+            {
+                Console.WriteLine(CommandLine.Text.HelpText.AutoBuild(options).ToString());
+                return -1;
+            }
+
+
+            string zpl_file_loc = options.ZPL_File;
+            string zpl_frmt = File.ReadAllText(zpl_file_loc);
+
+            string data_file_loc = options.Data_File;
+            string[] data = new string[] { };
+            string[] stringSeparators = new string[] { "\r\n" };
+            if (data_file_loc != null)
+            {
+                string datastr = File.ReadAllText(data_file_loc);
+                data = datastr.Split(stringSeparators, StringSplitOptions.None);
+            }
+
+
+            string ipAddress = options.Printer_Address;
+            int port = options.Printer_Port;
+
+            // Open connection
+            System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient();
+            client.Connect(ipAddress, port);
+
+            // Write ZPL String to connection
+            System.IO.StreamWriter writer =
+                new System.IO.StreamWriter(client.GetStream());
+
+            //object[] data = new object[3] { uid, mac, boardid };
+            string label = string.Format(zpl_frmt, data );
+
+            writer.Write(label);
+            writer.Flush();
+
+            // Close Connection
+            writer.Close();
+            client.Close();
+
+
+            return 0;
         }
     }
 }
